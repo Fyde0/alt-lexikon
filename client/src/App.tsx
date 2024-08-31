06852f90652d0230e29ef56361b621a351386f8e
Federico <fyde90@gmail.com>
2024-08-31 13:12:03 +0200

    setup client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom/client"
+
+function App() {
+
+  return (
+    <>
+      a
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+root.render(<App />)
