3276ed284e9d98e6840f157c0729fdb6da325416
Federico <fyde90@gmail.com>
2024-08-31 14:35:07 +0200

    setup mantine, fonts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,24 @@
 import ReactDOM from "react-dom/client"
+import { createTheme, MantineProvider } from "@mantine/core"
+import "@mantine/core/styles.css"
+import "./assets/css/index.css"
+// 
+import Root from "./pages/Root"
 
 function App() {
 
-  return (
-    <>
-      a
-    </>
-  )
+    const theme = createTheme({
+        fontFamily: 'Montserrat, sans-serif'
+    })
+
+    return (
+        <MantineProvider
+            defaultColorScheme="dark"
+            theme={theme}
+        >
+            <Root />
+        </MantineProvider>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
---
+++ client/src/pages/Root.tsx
@@ -0,0 +1,29 @@
+import { AppShell, Flex, Group, Title } from "@mantine/core"
+
+function Root() {
+
+    const maxWidth = "500px"
+
+    return (
+        <AppShell
+            header={{ height: { base: 60, md: 70, lg: 80 } }}
+            maw={maxWidth} p="md" mx="auto"
+        >
+
+            <AppShell.Header>
+                <Group h="100%" maw={maxWidth} px="md" mx="auto">
+                    <Title order={1} size="h3" fw={400}>
+                        <Flex align="center" gap="sm">
+                            <i className="fa-solid fa-book"></i> <span>Alt-Lexikon</span>
+                        </Flex>
+                    </Title>
+                </Group>
+            </AppShell.Header>
+
+            <AppShell.Main>Main</AppShell.Main>
+
+        </AppShell>
+    )
+}
+
+export default Root
